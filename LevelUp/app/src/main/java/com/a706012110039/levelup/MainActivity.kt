0fd62fd888960bb37418c35b04b99236a17955f5
dhammikod <63945345+dhammikod@users.noreply.github.com>
2022-10-12 20:03:36 +0700

    main activity
    aosdfahd

---
+++ LevelUp/app/src/main/java/com/a706012110039/levelup/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.a706012110039.levelup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
