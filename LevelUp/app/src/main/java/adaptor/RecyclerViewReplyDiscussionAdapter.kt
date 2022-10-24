ef67edf7a82c79a5f04a27281357b123d31cbe5d
v1nccc <vfarellio@student.ciputra.ac.id>
2022-10-25 01:51:03 +0700

    c
    C

--- LevelUp/app/src/main/java/adaptor/RecyclerViewReplyDiscussionAdapter.kt
+++ LevelUp/app/src/main/java/adaptor/RecyclerViewReplyDiscussionAdapter.kt
@@ -1,4 +1,60 @@
 package adaptor
 
-class RecyclerViewReplyDiscussionAdapter {
+import Database.GlobalVar
+import Interface.CardListener
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import com.a706012110039.levelup.R
+import model.discussion
+import model.reply
+
+class RecyclerViewReplyDiscussionAdapter (private val dataSet: ArrayList<reply>, val cardListener: CardListener) :
+    RecyclerView.Adapter<RecyclerViewReplyDiscussionAdapter.ViewHolder>() {
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val message: TextView
+        val pic: ImageView
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            message = view.findViewById(R.id.replymesssagetv)
+            pic = view.findViewById(R.id.replyppiv)
+
+        }}
+
+        // Create new views (invoked by the layout manager)
+        override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+            // Create a new view, which defines the UI of the list item
+            val view = LayoutInflater.from(viewGroup.context)
+                .inflate(R.layout.cardview_replies, viewGroup, false)
+
+            return ViewHolder(view)
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+            // Get element from your dataset at this position and replace the
+            // contents of the view with that element
+            viewHolder.message.text = dataSet[position].message
+//            viewHolder.pic.setImageURI(GlobalVar.users[dataSet[position].user].)
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        override fun getItemCount() = dataSet.size
+
+
+
 }
--- LevelUp/app/src/main/java/com/a706012110039/levelup/ReplyDiscussion.kt
+++ LevelUp/app/src/main/java/com/a706012110039/levelup/ReplyDiscussion.kt
@@ -1,11 +1,18 @@
 package com.a706012110039.levelup
 
 import Database.GlobalVar
+import Interface.CardListener
+import adaptor.RecyclerViewProjectDsicussionsAdapter
+import adaptor.RecyclerViewReplyDiscussionAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.a706012110039.levelup.databinding.ActivityProjectBinding
 import com.a706012110039.levelup.databinding.ActivityReplyDiscussionBinding
+import kotlinx.android.synthetic.main.activity_reply_discussion.*
 import kotlinx.android.synthetic.main.cardview_discussions.*
+import kotlinx.android.synthetic.main.fragment_project_discussion.*
+import kotlinx.android.synthetic.main.fragment_project_tasks.*
 
 class ReplyDiscussion : AppCompatActivity() {
     private lateinit var binding:ActivityReplyDiscussionBinding
@@ -14,6 +21,7 @@ class ReplyDiscussion : AppCompatActivity() {
         binding = ActivityReplyDiscussionBinding.inflate(layoutInflater)
         setContentView(binding.root)
         GetIntent()
+        setupRecycler()
     }
 
     private fun GetIntent(){
@@ -21,5 +29,12 @@ class ReplyDiscussion : AppCompatActivity() {
         binding.discussiontitletva.text = GlobalVar.projects[GlobalVar.projects.size-1].discussion[position].title
         binding.disucussionmakertv.text = GlobalVar.users[GlobalVar.projects[GlobalVar.projects.size-1].discussion[position].creator].name
         binding.discussionrepdesctv.text = GlobalVar.projects[GlobalVar.projects.size-1].discussion[position].description
+    }
+
+    fun setupRecycler(){
+
+       val layoutManager = GridLayoutManager(baseContext, 1)
+        repliesrv.layoutManager = layoutManager
+        projectdiscussionsrv.adapter = adapter.sa
     }
 }
