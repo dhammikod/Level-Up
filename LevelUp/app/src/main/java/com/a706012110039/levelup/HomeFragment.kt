a4674f6140197abbeee9981b3f15186f74e21874
Hagen-Kwik <hagen.kwik@gmail.com>
2022-11-15 22:04:13 +0700

    upcoming

--- LevelUp/app/src/main/java/com/a706012110039/levelup/HomeFragment.kt
+++ LevelUp/app/src/main/java/com/a706012110039/levelup/HomeFragment.kt
@@ -5,6 +5,8 @@ import adaptor.RecyclerViewNewsAdaptor
 import adaptor.recyclerviewRecentsAdaptor
 import adaptor.recyclerviewUpcomingTaskAdaptor
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -17,6 +19,13 @@ import com.denzcoskun.imageslider.models.SlideModel
 import model.projects
 import model.user
 import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.MPPointF
+import kotlinx.android.synthetic.main.fragment_home.*
 import model.news
 
 class HomeFragment : Fragment() {
@@ -31,7 +40,7 @@ class HomeFragment : Fragment() {
         upcomingtask()
         tesads()
         tesnews()
-
+        piechart()
 
         return viewbind.root
     }
@@ -47,6 +56,89 @@ class HomeFragment : Fragment() {
         newsAdaptor = RecyclerViewNewsAdaptor(listnews)
         viewbind.recyclerviewnews.layoutManager = LinearLayoutManager(requireActivity().baseContext, LinearLayoutManager.VERTICAL, false)
         viewbind.recyclerviewnews.adapter = newsAdaptor
+    }
+
+    fun piechart(){
+        var pieChart = viewbind.pieChart
+
+        // on below line we are setting user percent value,
+        // setting description as enabled and offset for pie chart
+        pieChart.setUsePercentValues(true)
+        pieChart.getDescription().setEnabled(false)
+        pieChart.setExtraOffsets(5f, 10f, 5f, 5f)
+
+        // on below line we are setting drag for our pie chart
+        pieChart.setDragDecelerationFrictionCoef(0.95f)
+
+        // on below line we are setting hole
+        // and hole color for pie chart
+        pieChart.setDrawHoleEnabled(true)
+        pieChart.setHoleColor(Color.WHITE)
+
+        // on below line we are setting circle color and alpha
+        pieChart.setTransparentCircleColor(Color.WHITE)
+        pieChart.setTransparentCircleAlpha(110)
+
+        // on  below line we are setting hole radius
+        pieChart.setHoleRadius(58f)
+        pieChart.setTransparentCircleRadius(61f)
+
+        // on below line we are setting center text
+        pieChart.setDrawCenterText(true)
+
+        // on below line we are setting
+        // rotation for our pie chart
+        pieChart.setRotationAngle(0f)
+
+        // enable rotation of the pieChart by touch
+        pieChart.setRotationEnabled(true)
+        pieChart.setHighlightPerTapEnabled(true)
+
+        // on below line we are setting animation for our pie chart
+        pieChart.animateY(1400, Easing.EaseInOutQuad)
+
+        // on below line we are disabling our legend for pie chart
+        pieChart.legend.isEnabled = false
+        pieChart.setEntryLabelColor(Color.WHITE)
+        pieChart.setEntryLabelTextSize(12f)
+
+        // on below line we are creating array list and
+        // adding data to it to display in pie chart
+        val entries: ArrayList<PieEntry> = ArrayList()
+        entries.add(PieEntry(60f))
+        entries.add(PieEntry(40f))
+
+
+        // on below line we are setting pie data set
+        val dataSet = PieDataSet(entries, "Mobile OS")
+
+        // on below line we are setting icons.
+        dataSet.setDrawIcons(false)
+
+        // on below line we are setting slice for pie
+        dataSet.sliceSpace = 3f
+        dataSet.iconsOffset = MPPointF(0f, 40f)
+        dataSet.selectionShift = 5f
+
+        // add a lot of colors to list
+        val colors: ArrayList<Int> = ArrayList()
+        colors.add(resources.getColor(R.color.lightbluebg))
+        colors.add(resources.getColor(R.color.purple_200))
+
+        dataSet.colors = colors
+
+        // on below line we are setting pie data set
+        val data = PieData(dataSet)
+        data.setValueFormatter(PercentFormatter())
+        data.setValueTypeface(Typeface.DEFAULT_BOLD)
+        data.setValueTextColor(Color.WHITE)
+        pieChart.setData(data)
+
+        // undo all highlights
+        pieChart.highlightValues(null)
+
+        // loading chart
+        pieChart.invalidate()
     }
 
     fun tesads(){
