246f190bbde50a894f9372d1afb1925d6cc2846a
Kevin <kchristian03@student.ciputra.ac.id>
2022-11-15 12:11:07 +0700

    id design jobs

--- LevelUp/app/src/main/java/com/a706012110039/levelup/LoginActivity.kt
+++ LevelUp/app/src/main/java/com/a706012110039/levelup/LoginActivity.kt
@@ -1,12 +1,16 @@
 package com.a706012110039.levelup
 
 import Database.GlobalVar
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.a706012110039.levelup.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var shared: SharedPreferences
+    var remember = false
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,5 +32,18 @@ class LoginActivity : AppCompatActivity() {
             GlobalVar.curuser = 0
             startActivity(Intent)
         }
+    }
+
+    fun checkRemember() {
+        shared = getSharedPreferences("shared", Context.MODE_PRIVATE)
+        remember = shared.getBoolean("remember", false)
+
+        if(remember){
+//            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, BottomnavbarActiity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
